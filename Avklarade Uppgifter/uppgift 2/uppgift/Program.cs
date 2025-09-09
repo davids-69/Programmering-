0d5cd964a19a324f130d1c52083d4eda6c466d83
Lucas Klevvall <ht24kllu@axevalla.fhsk.se>
2025-09-09 12:06:11 +0200

    uppgift 2 och 3

---
+++ Avklarade Uppgifter/uppgift 2/uppgift/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is jeff");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
+
---
+++ Avklarade Uppgifter/uppgift 3/uppgift 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentYear = 2025;
+            Console.WriteLine("uppgift 2");
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
+            Console.WriteLine("hur gammal är du?");
+            string age = Console.ReadLine();
+
+            
+        }
+    }
+}
