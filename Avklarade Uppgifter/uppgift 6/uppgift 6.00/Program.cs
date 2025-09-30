61c1d088a796de071848ce69f9dc16ed1d8912bd
davids-69 <sspel25dasa@axevalla.fhsk.se>
2025-09-30 10:50:47 +0200

    Add files via upload

---
+++ Avklarade Uppgifter/uppgift 6/uppgift 6.00/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_6._00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            {
+                int belopp = 100;
+                int val = 0;
+                while (belopp > 0)
+                {
+                    Console.WriteLine("Du har " + belopp + " kr kvar.");
+                    Console.WriteLine("Liten glass: 25kr\nMellan glass: 50kr\nStor glass: 75kr");
+                    Console.WriteLine("Välj ett alternativ:");
+                    val = int.Parse(Console.ReadLine());
+                    switch (val)
+                    {
+                        case 1:
+                            belopp -= 25;
+                            Console.WriteLine("Du köpte en liten glass.");
+                            break;
+
+                        case 2:
+                            belopp -= 50;
+                            Console.WriteLine("Du köpte en mellan glass.");
+                            break;
+
+                        case 3:
+                            belopp -= 75;
+                            Console.WriteLine("Du köpte en stor glass.");
+                            break;
+
+
+                        default:
+                            break;
+                    }
+
+
+
+
+
+
+                    /*
+                    if (val == 1)
+                    {
+                        belopp -= 25;
+                        Console.WriteLine("Du köpte en liten glass.");
+                    }
+                    else if (val == 2)
+                    {
+                        belopp -= 50;
+                        Console.WriteLine("Du köpte en mellan glass.");
+                    }
+                    else if (val == 3)
+                    {
+                        belopp -= 75;
+                        Console.WriteLine("Du köpte en stor glass.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Felaktigt val, försök igen.");
+                    }*/
+                }
+            }
+        }
+    }
+}
+
