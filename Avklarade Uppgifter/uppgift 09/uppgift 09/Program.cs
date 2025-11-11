0dc98989cf79ed4d1d4fc337f00ee42a7b0bee03
unknown <sspel25dasa@axevalla.fhsk.se>
2025-11-11 11:31:20 +0100

    uppgift 9

---
+++ Avklarade Uppgifter/uppgift 09/uppgift 09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_09
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //string[] namn = { "saul", "tony", "paul" };
+            string[] namn = new string[3]
+            for (int index = 0; index < namn.Length; index++)
+            {
+                Console.WriteLine(namn[index]);
+            }
+        }
+    }
+}
