9927ea73d11fb16077e864db07328355f2a60667
Pancake <sspel25adni@axevalla.fhsk.se>
2025-12-02 11:45:33 +0100

    uppgift 9 klar

--- Avklarade Uppgifter/uppgift 09/uppgift 09/Program.cs
+++ Avklarade Uppgifter/uppgift 09/uppgift 09/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,11 +12,42 @@ namespace uppgift_09
         
         static void Main(string[] args)
         {
-            //string[] namn = { "saul", "tony", "paul" };
-            string[] namn = new string[3]
+            Console.WriteLine("skriv 3 olika namn");
+            string[] namn = new string[3];
             for (int index = 0; index < namn.Length; index++)
             {
-                Console.WriteLine(namn[index]);
+                namn[index] = Console.ReadLine();
+            }
+            while(true)
+            {
+
+            
+                Console.WriteLine("skriv in 1, 2, 3 eller 0 for lista");
+           
+                int val = int.Parse(Console.ReadLine());
+                if (val == 1)
+                {
+                    Console.WriteLine("personen heter " + namn[0]);
+                }
+                else if (val == 2)
+                {
+                    Console.WriteLine("personen heter " + namn[1]);
+                }
+                else if (val == 3)
+                {
+                    Console.WriteLine("personen heter " + namn[2]);
+                }
+                else if (val == 0)
+                {
+                    for (int index = 0; index < namn.Length; index++)
+                    {
+                        Console.WriteLine(namn[index]);
+                        
+
+                    }
+                    break;
+                }
+                  //  Console.WriteLine("0 for lista" + " " + namn[0] + " " + namn[1] + " " + namn[2]);
             }
         }
     }
