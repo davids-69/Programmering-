be8672140c00f522ce680a998cef26d7833966fa
davids-69 <sspel25dasa@axevalla.fhsk.se>
2026-03-11 11:32:52 +0100

    uppgift 10

--- Uppgift 10 - Enarmad bandit (ej färdig)/Uppgift 10/Program.cs
+++ Uppgift 10 - Enarmad bandit (ej färdig)/Uppgift 10/Program.cs
@@ -8,22 +8,38 @@ namespace Uppgift_10
 
         static void Main(string[] args)
         {
-            int credits = 100;
-
+            
+            int credits = 0;
             Random random = new Random();
-            string[] symbols = new[] { "CHERRY", "BELL", "LEMON", "STAR", "7" };
+            credits = random.Next(10, 1001);
+
+
+            Console.ReadLine();
+            string[] symbols = new[] { "CHERRY", "LEMON", "7" };
             Console.WriteLine("Välkommen till Slot Machine Game!");
-            Console.WriteLine($"Du börjar med {credits} krediter.");
+          //  Console.WriteLine($"Du börjar med {credits} krediter.");
             Console.WriteLine("Tryck på valfri tangent för att spela...");
+            credits = int.Parse(Console.ReadLine());
+            Console.WriteLine("Du har nu möljiheten att sätta in dina krediter, skriv in hur många krediter du vill sätta in:");
+
+            Console.WriteLine("du har nu satt in credits och är redo att spela");
+
 
             while (true)
             {
-                if (credits <= 0)
+                if (credits >= 0)
+                {
+                    Console.WriteLine(symbols[random.Next(0, 3)] + symbols[random.Next(0, 3)] + symbols[random.Next(0, 3)]);
+
+                }
+
+                else
                 {
                     Console.WriteLine("Du har inga krediter");
 
                 }
             }
+            
         }
        
     }
