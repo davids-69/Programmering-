a5fed6b790d4ed0dc2ef1af8d1f9f0d7d3aa9779
Axevalla <sspel25juwa@axevalla.fhsk.se>
2026-04-08 11:44:02 +0200

    uppgift 11

--- Uppgift 11 - klasser/Uppgift 11/Program.cs
+++ Uppgift 11 - klasser/Uppgift 11/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,35 +8,78 @@ using System.Threading.Tasks;
 namespace Uppgift_11
 {
 
-    
+
     internal class Program
     {
         public class Animals
         {
-            string animals;
-            string Type;
-            int choose;
-            
+
+            public string animals;
+            public string diet;
+            public int numberOfLegs;
+
+
+            public void animalInfo()
+            {
+                Console.WriteLine("Animal name: " + animals);
+                Console.WriteLine("Animal food: " + diet);
+                Console.WriteLine("Animal Legs: " + numberOfLegs);
+            }
+
         }
         static void Main(string[] args)
         {
-            List<Animals> Animals = new List<Animals>();
+
+            // animals.animalInfo();
             Animals animals = new Animals();
-            Animals.Add(animals);
+
+            int choose = 0;
+            choose = 0;
+            List<Animals> Animals = new List<Animals>();
 
             while (true)
             {
-                Console.WriteLine("choose a animal");
-                Console.ReadKey();
-            }
-            if (Animals.Count == 0)
-            {
-                Console.WriteLine("animal amount");
-                Console.ReadKey();
-                animals = new Animals();
 
 
+                while (choose <= 0 || choose >= 4)
+                {
+                    Console.WriteLine("Choose 1 - 3");
+                    choose = Convert.ToInt32(Console.ReadLine());
+
+
+                }
+                if (choose == 1)
+                {
+
+                    animals = new Animals();
+                    Console.WriteLine("choose a animal");
+                    animals.animals = Console.ReadLine();
+                    Console.WriteLine("what does the animal eat");
+                    animals.diet = Console.ReadLine();
+                    Console.WriteLine("number of legs");
+                    animals.numberOfLegs = Convert.ToInt32(Console.ReadLine());
+                    Animals.Add(animals);
+                }
+                if (choose == 2)
+                {
+
+                    Console.WriteLine("list of animals");
+                    for (int i = 0; i < Animals.Count; i++)
+                    {
+
+                        Animals[i].animalInfo();
+                    }
+                }
+                else if (choose == 3)
+                {
+                    break;
+                }
+                choose = 0;
+                 
             }
         }
     }
 }
+                     
+
+
