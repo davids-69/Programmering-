068be24686d5a7f24ea6a42cf802d6282435487b
davids-69 <sspel25dasa@axevalla.fhsk.se>
2026-05-05 11:42:53 +0200

    uppgift 12

--- Uppgift 12 - Skriva till- och från fil/FileManagement/Program.cs
+++ Uppgift 12 - Skriva till- och från fil/FileManagement/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Diagnostics;
 
 namespace FileManagement
 {
@@ -12,22 +13,32 @@ namespace FileManagement
         static void Main(string[] args)
         {
             //Hämta information ifrån textfilen! Denna fil finns lokalt på datorn.
-            string input01 = File.ReadAllText("../../Tim's mapp/File00.txt");
-            //string input02 = ??? (ReadAllText)
-            //string[] input03 = ??? (ReadAllLines)
-            //string[] input04 = ??? (ReadAllLines)
-
+            //string input01 = File.ReadAllText("../../Tim's mapp/File00.txt");
+            string input01 = File.ReadAllText("C:\\Users\\Axevalla\\Desktop\\Programmering-\\Uppgift 12 - Skriva till- och från fil\\FileManagement\\FilesToRead\\File01.txt");
+            string input02 = File.ReadAllText("C:\\Users\\Axevalla\\Desktop\\Programmering-\\Uppgift 12 - Skriva till- och från fil\\FileManagement\\FilesToRead\\File02.txt");
+            string[] input03 = File.ReadAllLines("C:\\Users\\Axevalla\\Desktop\\Programmering-\\Uppgift 12 - Skriva till- och från fil\\FileManagement\\FilesToRead\\File03.txt");
+            string[] input04 = File.ReadAllLines("C:\\Users\\Axevalla\\Desktop\\Programmering-\\Uppgift 12 - Skriva till- och från fil\\FileManagement\\FilesToRead\\File04.txt");
             //Jobba med File01 här
             string result01 = File01Task(input01);
             Console.WriteLine(result01);
 
             //Jobba med File02 här
-            //Kalla på funktion
+            List<int> result02 = File02Task(input02);
+            foreach(int siffra in result02)
+            {
+                Console.WriteLine(siffra);
+            }
 
             //Jobba med File03 här
-            //Kalla på funktion
-
+            int[] result03 = File03Task(input03);
+            Console.WriteLine("Result 03:");
+            foreach (int siffra in result03)
+            {
+                Console.WriteLine(siffra);
+            }
             //Jobba med File04 här
+            string result04 = File04Task(input04);
+            Console.WriteLine(result04);
             //Kalla på funktion
             //Skriv till textdokumentet
 
@@ -43,7 +54,11 @@ namespace FileManagement
             char[] listToSort = input.ToCharArray();
             string returnString = "";
 
-            foreach(char letter in listToSort)
+            //Sortera arrayen här.
+            Array.Sort(listToSort);
+
+
+            foreach (char letter in listToSort)
             {
                 returnString += letter;
             }
@@ -59,6 +74,8 @@ namespace FileManagement
         {
             string[] listToSort = input.Split(',');
 
+            
+
             //Vi vill mata in vår lista av "bokstavssiffror" så att de blir till
             //faktiska siffror. Vi använder en lista här istället för en array eftersom
             //vi kommer att vilja lägga till saker dynamiskt. 
@@ -66,8 +83,10 @@ namespace FileManagement
 
             foreach(string siffra in listToSort)
             {
+                intsInList.Add(int.Parse(siffra));
                 //För varje (for each) siffra (vårt valda variabelnamn) i vår lista
                 //ska läggas in i vår lista "intsInList". 
+
             }
 
             return intsInList;
@@ -75,20 +94,23 @@ namespace FileManagement
 
         static int[] File03Task(string[] input)
         {
-            //Här får ni en färdig array med rader som enbart består av "bokstavssiffror".
-            //Ert uppdrag är att sortera arrayen och skicka tillbaka den till main.
-
-            //Börja med att konvertera varje array element till en siffra och spara i en List<int>.
-
-            //Sortera listan!
-
-            //Returnera listan!
-            //Ersätt null med ert resultat!
-            return null;
+            List<int> intsInList = new List<int>();
+            foreach (string siffra in input)
+            {
+                intsInList.Add(int.Parse(siffra));
+            }
+            intsInList.Sort();
+            return intsInList.ToArray();
         }
 
-        static void File04Task(string[] input)
+        static string File04Task(string[] input)
         {
+            Console.WriteLine(input);
+            foreach(string line in input)
+            {
+                Console.WriteLine(line);
+                
+            }
             //Svara på frågorna! 
             //Ledtråd: jobba med varje array-element för sig.
             
@@ -96,7 +118,9 @@ namespace FileManagement
             //return? Just nu är funktionen "void" -- dvs. funktionen skickar inte tillbaka 
             //något värde.
             //Vill ni skicka tillbaka en string eller en List<string?/char?>?
+            return "";
         }
+        
 
         static void File05Task(string[] input)
         {
